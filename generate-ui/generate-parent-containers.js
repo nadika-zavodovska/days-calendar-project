f317bba6fee78bb00280532455f4ab29f0c3c402
nadika <nadikazavodovska@gmail.com>
2025-02-09 13:33:46 +0000

    Create header

--- generate-ui/generate-parent-containers.js
+++ generate-ui/generate-parent-containers.js
@@ -1,3 +1,5 @@
 export function generateParentContainers() {
-   
+    const headerContainer = document.createElement('header');
+    headerContainer.id = "header-container";
+    document.body.appendChild(headerContainer);
 }
