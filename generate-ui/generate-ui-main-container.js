1090d3145200e80eb5cf3b51b91394e2e9f02bed
nadika <nadikazavodovska@gmail.com>
2025-02-10 10:10:27 +0000

    Add back, next btn, current date

--- generate-ui/generate-ui-main-container.js
+++ generate-ui/generate-ui-main-container.js
@@ -14,4 +14,19 @@ export function generateUiMainContainer() {
     const backNextBtnSection = document.createElement("section");
     backNextBtnSection.id = "back-next-btn-section";
     mainContainer.appendChild(backNextBtnSection);
+
+    const prevMonthBtn = document.createElement("button");
+    prevMonthBtn.id = "prev-month";
+    backNextBtnSection.appendChild(prevMonthBtn);
+    prevMonthBtn.innerText = "Back";
+
+    const nextMonthBtn = document.createElement("button");
+    nextMonthBtn.id = "next-month";
+    backNextBtnSection.appendChild(nextMonthBtn);
+    nextMonthBtn.innerText = "Next";
+
+    const currentDate = document.createElement("section");
+    currentDate.id = "current-date";
+    mainContainer.appendChild(currentDate);
+    currentDate.innerText = "10 February 2025"; 
 }
