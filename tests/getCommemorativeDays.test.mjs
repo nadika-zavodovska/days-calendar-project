6902a99b487ef1d70c36fad8063efb845261866b
nadika <nadikazavodovska@gmail.com>
2025-02-12 19:03:42 +0000

    Add test

--- tests/getCommemorativeDays.test.mjs
+++ tests/getCommemorativeDays.test.mjs
@@ -10,6 +10,16 @@ describe("getCommemorativeDay function", () => {
             "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/binturongs.txt"
         })).toBe(11);
     });
+
+    test("should return the correct day for Ada Lovelace Day when October 2024 is selected", () => {
+        expect(getCommemorativeDay(2024, 9, {
+            "name": "Ada Lovelace Day",
+            "monthName": "October",
+            "dayName": "Tuesday",
+            "occurrence": "second",
+            "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/ada.txt"
+        })).toBe(8);
+    });
 });
 
 
