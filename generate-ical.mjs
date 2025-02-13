8294c412aa3b6f0ba647c34d8589ebdb233f0e40
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-02-13 11:22:57 +0000

    Update generate-ical.mjs

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,11 +1,3 @@
-// This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
-// It can be run with `node`.
-
-
-//import daysData from "./days.json" with { type: "json" };
-
-//console.log(`{getGreeting()} - there are ${daysData.length} known days`);
-
 import fs from 'fs';
 
 // Function to get the weekday index (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
