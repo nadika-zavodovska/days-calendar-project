0d77316067c257195c1e5752a9e657fe87bab2be
nadika <nadikazavodovska@gmail.com>
2025-02-13 23:24:21 +0000

    Add tests

--- tests/getCommemorativeDay.test.mjs
+++ tests/getCommemorativeDay.test.mjs
@@ -1,16 +1,6 @@
 import { getCommemorativeDay } from '../src/common.mjs';
 
 describe("getCommemorativeDay function", () => {
-    test("should return the correct day for International Binturong Day when May 2030 is selected", () => {
-        expect(getCommemorativeDay(2030, 4, {
-            "name": "International Binturong Day",
-            "monthName": "May",
-            "dayName": "Saturday",
-            "occurrence": "second",
-            "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/binturongs.txt"
-        })).toBe(11);
-    });
-
     test("should return the correct day for Ada Lovelace Day when October 2024 is selected", () => {
         expect(getCommemorativeDay(2024, 9, {
             "name": "Ada Lovelace Day",
@@ -21,6 +11,16 @@ describe("getCommemorativeDay function", () => {
         })).toBe(8);
     });
 
+    test("should return the correct day for Ada Lovelace Day when October 2024 is selected", () => {
+        expect(getCommemorativeDay(2025, 9, {
+            "name": "Ada Lovelace Day",
+            "monthName": "October",
+            "dayName": "Tuesday",
+            "occurrence": "second",
+            "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/ada.txt"
+        })).toBe(14);
+    });
+
     test("should return the correct day for Ada World Lemur Day when October 2024 is selected", () => {
         expect(getCommemorativeDay(2024, 9, {
             "name": "World Lemur Day",
@@ -29,6 +29,36 @@ describe("getCommemorativeDay function", () => {
             "occurrence": "last",
             "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/lemurs.txt"
         })).toBe(25);
+    });
+
+    test("should return the correct day for Ada Lovelace Day when October 2020 is selected", () => {
+        expect(getCommemorativeDay(2020, 9, {
+            "name": "Ada Lovelace Day",
+            "monthName": "October",
+            "dayName": "Tuesday",
+            "occurrence": "second",
+            "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/ada.txt"
+        })).toBe(13);
+    });
+
+    test("should return the correct day for Ada World Lemur Day when October 2020 is selected", () => {
+        expect(getCommemorativeDay(2020, 9, {
+            "name": "World Lemur Day",
+            "monthName": "October",
+            "dayName": "Friday",
+            "occurrence": "last",
+            "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/lemurs.txt"
+        })).toBe(30);
+    });
+
+    test("should return the correct day for International Binturong Day when May 2030 is selected", () => {
+        expect(getCommemorativeDay(2030, 4, {
+            "name": "International Binturong Day",
+            "monthName": "May",
+            "dayName": "Saturday",
+            "occurrence": "second",
+            "descriptionURL": "https://codeyourfuture.github.io/The-Piscine/days/binturongs.txt"
+        })).toBe(11);
     });
 });
 
